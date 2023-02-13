768798e0b81afaf3c25dd9b7c0effdad4ced3f6e
iambasilp <basilxg321@gmail.com>
2023-02-13 20:29:20 +0530

    navbar banner section important foramtion updated

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu')
+
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
